6f5afc9aa6f6da84f1e936b96724836b4d83dcee
kushwaha03 <kushwahakrishna03@gmail.com>
2019-01-16 17:35:02 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # root 'static_pages#home'
   #
   # get 'static_pages/home'
@@ -7,13 +6,16 @@ Rails.application.routes.draw do
   # get  'static_pages/about'
   # get 'static_pages/contact'
   Rails.application.routes.draw do
-  get 'users/new'
     root 'static_pages#home'
     # get  '/home',    to: 'static_pages#home'
     get  '/help',    to: 'static_pages#help'
     get  '/about',   to: 'static_pages#about'
     get  '/contact', to: 'static_pages#contact'
     get  '/signup',  to: 'users#new'
+    post '/signup',  to: 'users#create'
+
+    resources :users
+
   end
 
   # root 'static_pages#home'
